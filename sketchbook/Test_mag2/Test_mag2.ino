acc813ff1cdd8e2d2a24dc3e6a8a8d8acc0105b3
Phil Wilshire <sysdcs@gmail.com>
2014-11-25 06:23:23 -0500

    added sketchbook

---
+++ sketchbook/Test_mag2/Test_mag2.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <Adafruit_LSM303.h>
+int led = 7;
+int accel_x;
+Adafruit_LSM303 lsm;
+
+void setup() 
+{
+  Serial.begin(9600);
+    // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);  
+  // Try to initialise and warn if we couldn't detect the chip
+  if (!lsm.begin())
+  {
+    Serial.println("Oops ... unable to initialize the LSM303. Check your wiring!");
+    while (1);
+  }
+}
+
+void loop() 
+{
+  lsm.read();
+  Serial.print("Accel X: "); Serial.print((int)lsm.accelData.x); Serial.print(" ");
+  Serial.print("Y: "); Serial.print((int)lsm.accelData.y);       Serial.print(" ");
+  Serial.print("Z: "); Serial.println((int)lsm.accelData.z);     Serial.print(" ");
+  Serial.print("Mag X: "); Serial.print((int)lsm.magData.x);     Serial.print(" ");
+  Serial.print("Y: "); Serial.print((int)lsm.magData.y);         Serial.print(" ");
+  Serial.print("Z: "); Serial.println((int)lsm.magData.z);       Serial.print(" ");
+  accel_x = lsm.accelData.x;
+  if (accel_x < -50) accel_x = -50;
+  if (accel_x > 50) accel_x = 50;
+  
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100 + accel_x);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(200+(accel_x *2));               // wait for a second
+  //delay(1000);
+}
+
