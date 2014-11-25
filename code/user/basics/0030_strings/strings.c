4af9cc589780617d950863ff4953b81545d428b0
Phil Wilshire <sysdcs@gmail.com>
2014-11-25 13:42:00 -0500

    added batch command

--- code/user/basics/0030_strings/strings.c
+++ code/user/basics/0030_strings/strings.c
@@ -9,24 +9,30 @@
  * other commands that may have started.
  *   
  * this code can be tested out on an x86 system.
+ * extending this to include the $$commit/$$clear/$$add/$$show and $$del 
+ * commands 
  *
  *****************************************************************/
  
 /* try to run this with 
 
 ./strings " command 1 \n command2 " , " some data", " done \n"
+
+./strings "cmd_2,this ,is, one, for mexx another command xx foo" " and xx one,more time,xx
 */
-  
 
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
 
-#define CMD_TERM "xx"
+#define CMD_TERM "\n"
 #define CMD_SEP ","
 #define NUM_TAGS 1024
+#define NUM_CMDS 1024
+int cmd_num = 0;
 char cmd_str[1024];
 char *cmd_tags[NUM_TAGS];
+char *cmd_list[NUM_CMDS];
 
 
 
@@ -34,7 +40,7 @@ char *cmd_tags[NUM_TAGS];
 typedef struct cmd_fcns {
   const char *key;
   int (*handler)(void *data, void *cdata, int argc, char **argv);
-  void * cdata;
+  void *cdata;
 } cmd_fcns_t;
 
 int run_cmd1(void *data, void *cdata, int cargc, char **cargv);
@@ -62,13 +68,13 @@ int run_cmd2(void * data, void *cdata, int cargc, char **cargv)
   return 0;
 }
 
-int run_cmd3(void * data, void *cdata, int cargc, char **cargv)
+int run_cmd3(void *data, void *cdata, int cargc, char **cargv)
 {
   printf(" cmd [%s] running argc (%d)\n", cargv[0], cargc);
   return 0;
 }
 
-int run_cmd4(void * data, void *cdata, int cargc, char **cargv)
+int run_cmd4(void *data, void *cdata, int cargc, char **cargv)
 {
   printf(" cmd [%s] running argc (%d)\n", cargv[0], cargc);
   return 0;
@@ -80,33 +86,39 @@ int decode_cmd(char *cmd_sp , void *data)
 {
   cmd_fcns_t *cmd;
   int ix = 0;
-  char *sp = cmd_sp;
+
   char *sep_sp;
+  char *sp;
+  char *cmd_save = strdup(cmd_sp);
+  printf(" running command [%s]\n", cmd_save);
+  sp = cmd_save;
+
   cmd_tags[ix]=sp;
   sep_sp = strstr(sp, CMD_SEP);
   while ((sep_sp != NULL) && ( ix < NUM_TAGS)) 
     {
       *sep_sp=0;
-      printf(" found cmd_tag (%d) [%s]\n", ix, cmd_tags[ix]);
+      //printf(" found cmd_tag (%d) [%s]\n", ix, cmd_tags[ix]);
       ix++;
       sp = sep_sp + strlen(CMD_SEP);
       cmd_tags[ix]=sp;
       sep_sp = strstr(sp, CMD_SEP);
     }
 
-  if ((ix > 0 ) || (ix == 0))
-      printf(" found cmd_tag (%d) [%s]\n", ix, cmd_tags[ix]);
+  //if ((ix > 0 ) || (ix == 0))
+  //    printf(" found cmd_tag (%d) [%s]\n", ix, cmd_tags[ix]);
   cmd = &cmds[0];
   while (cmd->key != NULL)
     {
       if(strcmp(cmd->key, cmd_tags[0])==0) 
 	{
-	  printf(" ***found command [%s] \n", cmd->key);
+	  //printf(" ***found command [%s] \n", cmd->key);
 	  cmd->handler(data, (void *)cmd, ix, cmd_tags);
 	  break;
 	}
       cmd++;
     }
+  free(cmd_save);
   return ix;
 }
 
@@ -116,7 +128,7 @@ int process_cmd(char *cmd_sp, int len, char *new_sp, void *data)
   char *term_sp;
   char *cmd_cpy;
   char *cmd_rep;
-
+  int i;
   if(new_sp)
     {
       snprintf (&cmd_sp[strlen(cmd_sp)]
@@ -127,17 +139,53 @@ int process_cmd(char *cmd_sp, int len, char *new_sp, void *data)
     {
       cmd_cpy=strdup(cmd_sp);
       cmd_cpy[term_sp-cmd_sp]=0;
+      // handle special commands here
+      //$$list
+      //$$del
+      if(strncmp(cmd_cpy, "$$list", strlen("$$list")) == 0 )
+	{
+
+	  printf( "command list\n");
+	  for(i=0; i < cmd_num; i++)
+	    {
+	      printf(" command [%d] [%s]\n",i ,cmd_list[i]);
+	    }
+	  printf( "\n\n");
+	}
+      else if(strncmp(cmd_cpy, "$$del", strlen("$$del")) == 0 )
+	{
+	  for(i=0; i < cmd_num; i++)
+	    {
+	      free(cmd_list[i]);
+	      cmd_list[i]=NULL;
+	    }
+	  cmd_num = 0;
+	  printf( "command list deleted\n");
+	}
+      else if(strncmp(cmd_cpy, "$$run", strlen("$$run")) == 0 )
+      {
+	  for(i=0; i < cmd_num; i++)
+	  {
+	      decode_cmd(cmd_list[i], data);
+	  }
+      }
+      else
+      {
+
+	  cmd_list[cmd_num] = cmd_cpy;
+	  cmd_num++;
+      }
       term_sp+=strlen(CMD_TERM);
       cmd_rep = cmd_sp;
       while(*term_sp) 
-	{
+      {
 	  *cmd_rep++ = *term_sp++;
-	}
+      }
       *cmd_rep = 0;
-      printf(" command found [%s] string left [%s] \n", cmd_cpy, cmd_sp);
+      //printf(" command found [%s] string left [%s] \n", cmd_cpy, cmd_sp);
     
-      decode_cmd(cmd_cpy, data);
-      free(cmd_cpy);
+      //decode_cmd(cmd_cpy, data);
+      //free(cmd_cpy);
       rc = 1;
     }
   return rc;
@@ -150,13 +198,14 @@ int main(int argc, char *argv[])
   int rc;
   memset (cmd_str, 0, sizeof(cmd_str));
   char *sp;
+  char buffer[1024];
   sp = &cmd_str[0];
   for (i = 1 ; i < argc; i++)
     {
       snprintf (&cmd_str[strlen(cmd_str)]
 		, sizeof(cmd_str) - strlen(cmd_str), "%s", argv[i]);
     }
-  printf(" working with cmd_str [%s] \n", cmd_str);
+  //printf(" working with cmd_str [%s] \n", cmd_str);
 
   cmd_str[0]=0;
   for (i = 1 ; i < argc; i++)
@@ -169,7 +218,13 @@ int main(int argc, char *argv[])
 	  sp = NULL;
 	}
     }
-  printf(" test all done \n");
+  //printf(" test all done \n");
+  while (fgets(buffer, sizeof (buffer), stdin))
+    {
+      //printf(" You entered [%s] \n" ,buffer);
+      rc = process_cmd(cmd_str, sizeof(cmd_str), buffer, NULL);
+      
+    } 
   return 0;
 }
 
